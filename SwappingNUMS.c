0b14bce6c96db05aac7321c08aa81a450975adf9
Cap312 <83711888+Cap312@users.noreply.github.com>
2021-08-17 11:33:01 +0530

    Create SwappingNUMS.c
    Swapping of two numbers Without the use of a temporary variable.

---
+++ SwappingNUMS.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a, b;
+  printf("Enter the two numbers to be swapped : \n");
+  printf("a = ");
+  scanf("%d",&a);
+  printf("b = ");
+  scanf("%d",&b);
+  a = a + b;
+  b = a - b;
+  a = a - b;
+  printf("The numbers after being swapped : \na = %d b = %d", a, b);
+  return 0;
+}
